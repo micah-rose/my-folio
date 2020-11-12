7124eb405a1c4d2282ea408c3586c691bce789fd
Micah Lund <micah.r.lund@gmail.com>
2020-11-12 09:52:24 -0700

    Moved my name and job title into their own component.

---
+++ src/components/Info.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Info = props => {
+    return (
+        <>
+            <div className="info">
+                <h1 style={{fontSize: "50px"}}>Micah Lund</h1>
+                <h3>Technical Developer</h3>
+            </div>
+        </>
+    )
+}
+
+export default Info;
--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,36 +1,40 @@
 import React from 'react';
 import main from '../images/main.jpg';
+import Info from './Info';
 
 const Picture = props => {
     return (
         <>
         <div width="200" style={{float: "left", marginLeft: "80px"}}>
-            <svg width="350" height="auto" viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <svg 
+                width="350" 
+                height="auto" 
+                viewBox="0 0 100 100" 
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg">
             <defs>
-              <pattern id="img" patternUnits="userSpaceOnUse" width="200" height="200">
+              <pattern 
+                id="img" 
+                patternUnits="userSpaceOnUse" 
+                width="200" 
+                height="200">
                 <image 
                   href={main} 
                   x="-25" 
                   width="150" 
                   height="110" 
                   className="image" 
-                  alt="Main"
-                />
+                  alt="Main"/>
               </pattern>
             </defs>
             <polygon 
                 points="50 1 95 25 95 75 50 99 5 75 5 25"
                 fill="url(#img)"/>
             </svg>
-            <div className="info">
-                <h1>Micah Lund</h1>
-                <p>Technical Developer</p>
-            </div>
+            <Info />
         </div>
         </>
     )
 }
-
-//<!-- "https://api.github.com/users/micah-rose/avatar_url" -->
 
 export default Picture;
