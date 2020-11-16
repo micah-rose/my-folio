d2bb16243829a9c65802e3354580ae8c031b31e7
Micah Lund <micah.r.lund@gmail.com>
2020-11-15 18:20:26 -0700

    Moved Hobbies to its own component.

---
+++ src/components/Hobbies.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hobbies = props => {
+    return (
+        <>
+            <h2>Hobbies/Interests</h2>
+            <p className="hobbies">Hobbies/Interests content</p>
+            <br />
+        </>
+    )
+}
+
+export default Hobbies;
--- src/containers/Page.js
+++ src/containers/Page.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Intro from '../components/Intro';
+import Hobbies from '../components/Hobbies';
 
 const Page = props => {
     return (
@@ -8,12 +9,8 @@ const Page = props => {
 <div id="about" className="container-fluid">
       <div className="row">
         <div className="col-sm-12">
-          <Intro />
-          <h2>
-            Hobbies/Interests
-          </h2>
-          <p className="hobbies">Hobbies/Interests content</p>
-          <br />
+            <Intro />
+            <Hobbies />
           <h2>My Story</h2>
           <p className="my-story">
             My Story content
