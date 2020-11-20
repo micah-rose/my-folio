68930ffb127a4703c49c11b5472e412e850e33b5
Micah Lund <micah.r.lund@gmail.com>
2020-11-19 18:52:45 -0700

    Completed the styling and content for my Intro component.

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,9 +5,17 @@ const Intro = () => {
         <>
             <h2 id="intro">Intro</h2>
             <br />
-            <p className="intro">
-                Introductory content
-            </p>
+            <div className="intro">
+                <p>Howdy!! Welcome to my online portfolio. </p>
+                <p>
+                    This site is where I will showcase who I am, what I've done, what I'm working on, and most of all... my journey in software and web development.
+                    <br />Everything on this landing page was designed by me using ReactJS components and styled with the help of Bootstrap.
+                </p>
+                <p>
+                    I hope you enjoy the content I have put together, but don't get too attached. This is an ever growing and improving portfolio that I will be
+                    <br />continuously expanding and downsizing to keep it modern, exciting, and relevant. 
+                </p>
+            </div>
         </>
     )
 }
