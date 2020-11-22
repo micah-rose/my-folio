3e9e104317d94815cd33750e0b241a4e4c396f34
Micah Lund <micah.r.lund@gmail.com>
2020-11-21 19:29:33 -0700

    Started an unordered list for my hobbies/interests, but I am thinking of ways to make this section more interesting.

--- src/components/Hobbies.js
+++ src/components/Hobbies.js
@@ -5,7 +5,15 @@ const Hobbies = () => {
         <>
             <h2 id="hobbies">Hobbies/Interests</h2>
             <div className="hobbies">
-                <p>Hobbies/Interests content</p>
+                <u style={{textDecoration: "none", listStyleType: "none", fontSize: "25px"}}>
+                    <li>Boxing</li>
+                    <li>Weightlifting</li>
+                    <li>Yoga</li>
+                    <li>Art</li>
+                    <li>Road Trips</li>
+                    <li>Watching Live Music</li>
+                    <li>Snowboarding</li>
+                </u>
             </div>
         </>
     )
