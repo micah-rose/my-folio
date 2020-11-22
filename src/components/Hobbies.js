4739165a160589794f79140c40f0b0309aea5bf9
Micah Lund <micah.r.lund@gmail.com>
2020-11-21 19:34:57 -0700

    Wrapped a couple components in divs to keep header spacing consistent throughout the page.

--- src/components/Hobbies.js
+++ src/components/Hobbies.js
@@ -3,17 +3,23 @@ import React from 'react';
 const Hobbies = () => {
     return (
         <>
-            <h2 id="hobbies">Hobbies/Interests</h2>
-            <div className="hobbies">
-                <u style={{textDecoration: "none", listStyleType: "none", fontSize: "25px"}}>
-                    <li>Boxing</li>
-                    <li>Weightlifting</li>
-                    <li>Yoga</li>
-                    <li>Art</li>
-                    <li>Road Trips</li>
-                    <li>Watching Live Music</li>
-                    <li>Snowboarding</li>
-                </u>
+            <div className="container">
+                <h2 id="hobbies">Hobbies/Interests</h2>
+                <div className="hobbies">
+                    <u style={{
+                        textDecoration: "none", 
+                        listStyleType: "none", 
+                        fontSize: "25px"}}
+                    >
+                        <li>Boxing</li>
+                        <li>Weightlifting</li>
+                        <li>Yoga</li>
+                        <li>Art</li>
+                        <li>Road Trips</li>
+                        <li>Watching Live Music</li>
+                        <li>Snowboarding</li>
+                    </u>
+                </div>
             </div>
         </>
     )
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -3,9 +3,11 @@ import React from 'react';
 const Skills = () => {
     return (
         <>
-            <h2 id="skills">Skills & Projects</h2>
-            <div className="skills">
-                <p>Skills & Projects content</p>
+            <div className="container">
+                <h2 id="skills">Skills & Projects</h2>
+                <div className="skills">
+                    <p>Skills & Projects content</p>
+                </div>
             </div>
         </>
     )
