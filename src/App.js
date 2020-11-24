07480fef840c6c4063e423641bce27e86c418e9a
Micah Lund <micah.r.lund@gmail.com>
2020-11-24 16:13:11 -0700

    Removed unused code.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
 import Nav from './containers/Nav';
 import Header from './containers/Header';
-// import Page from './containers/Page';
-// import Footer from './containers/Footer';
 
 function App() {
   return (
     <div className="App">
         <Nav />
         <Header />
-        {/* <Page />*/}
-        {/* <Footer />  */}
     </div>
   );
 }
