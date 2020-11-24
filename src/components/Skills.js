1a127e695694ee8f0af6dc30e87f969c7d7c93b4
Micah Lund <micah.r.lund@gmail.com>
2020-11-23 17:04:44 -0700

    Started adding content to the Skills & Projects section.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,7 +6,21 @@ const Skills = () => {
             <div className="container">
                 <h2 id="skills">Skills & Projects</h2>
                 <div className="skills">
-                    <p>Skills & Projects content</p>
+                <u style={{
+                        textDecoration: "none", 
+                        listStyleType: "none", 
+                        fontSize: "25px"}}
+                    >
+                        <li>JavaScript (<em>2+ years</em>)</li>
+                        <li>HTML/CSS (<em>2+ years</em>)</li>
+                        <li>ReactJS (<em>up to 1 year</em>)</li>
+                        <li>NodeJS (<em>in progress</em>)</li>
+                        <li>Java (<em>up to 1 year</em>)</li>
+                        <li>PHP (<em>less than 1 year</em>)</li>
+                        <li>Microsoft SQL Server (<em>3+ years</em>)</li>
+                        <li>MERN Stack (<em>in progress</em>)</li>
+                        <li>DevOps (<em>in progress</em>)</li>
+                    </u>
                 </div>
             </div>
         </>
