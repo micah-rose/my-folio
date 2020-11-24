fd4148b86d9c51bd6ee707597b776e99882ca849
Micah Lund <micah.r.lund@gmail.com>
2020-11-24 16:21:41 -0700

    Adjusted the text in the intro to balance it out.

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -7,7 +7,7 @@ const Intro = () => {
                 <p>
                     This site is where I will showcase who I am, what I've done, what I'm working on, and 
                     most of all... my journey in software and web development. Everything on this landing page 
-                    was designed by me using ReactJS components and styled with the help of Bootstrap.
+                    was designed by me using ReactJS and styled with the help of Bootstrap.
                 </p>
                 <p>
                     I hope you enjoy the content I have put together, but don't get too attached. This is an 
@@ -16,6 +16,7 @@ const Intro = () => {
                 </p>
                 <br />
                 <p>Fun Fact: the particle component you see above was inspired by a pair of yoga pants.</p>
+                <br />
             </div>
         </>
     )
