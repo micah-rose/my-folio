dad30e8c5ef58ff675f7e0196ed8d45729c6a32b
Micah Lund <micah.r.lund@gmail.com>
2020-11-25 12:53:25 -0700

    Updated the content in About Me.

--- src/containers/AboutMe.js
+++ src/containers/AboutMe.js
@@ -10,9 +10,10 @@ const AboutMe = () => {
             <h2 style={{fontSize: "80px"}}>About Me</h2>
             <p style={{fontSize: "25px", width: "80%", margin: "auto"}}>
             Hello there!! I'm Micah and I am the junior software engineer who built this site. I am 
-            excited you are here to learn more about the who, why, and where that make up
-            all things me (the what is demonstrated on my Skills & Projects section). Let's get started!!
+            excited you are here to learn more about me and my journey in software and web development. 
+            Let's get started!!
             </p>
+            <br />
             <hr />
             <Who />
             <Why />
