a22fec4d88630e16319961b9a70c43e36802320f
Micah Lund <micah.r.lund@gmail.com>
2020-11-25 18:00:40 -0700

    Began working on the Skills & Projects section.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,86 +1,14 @@
 import React from 'react';
-import js from '../images/js-logo.png';
-import css from '../images/css-logo.png';
-import html from '../images/html-logo.png';
+import What from './What';
 
 
 const Skills = () => {
     return (
         <>
-            <div className="container">
             <div className="skills">
             <h2 style={{fontSize: "80px"}}>Skills & Projects</h2>
             <hr />
-                <u style={{
-                        textDecoration: "none", 
-                        listStyleType: "none", 
-                        fontSize: "25px"}}
-                    >
-                        <li>JavaScript (<em>2+ years</em>)</li>
-                        <li>HTML/CSS (<em>2+ years</em>)</li>
-                        <li>ReactJS (<em>up to 1 year</em>)</li>
-                        <li>NodeJS (<em>in progress</em>)</li>
-                        <li>Java (<em>up to 1 year</em>)</li>
-                        <li>PHP (<em>less than 1 year</em>)</li>
-                        <li>Microsoft SQL Server (<em>3+ years</em>)</li>
-                        <li>MERN Stack (<em>in progress</em>)</li>
-                        <li>DevOps (<em>in progress</em>)</li>
-                    </u>
-                </div>
-                <br />
-                <h1 style={{fontWeight: "bold", color: "black"}}>My Computer Science Projects</h1>
-                <br />
-                <p style={{fontSize:"25px", marginBottom: "15px"}}>Click the below icons to display a list of courseworks for each section.</p>
-                    <div class="row">
-                        <div class="col-sm-4">
-                          <a href="#demo" data-toggle="collapse">
-                            <img src={html}  alt="HTML" width="200" height="200" />
-                          </a>
-                          <div id="demo" class="collapse">
-                            <ul style={{
-                                textDecoration: "none", 
-                                listStyleType: "none"}}>
-                                <li><a target="_blank" href="https://micah-rose.github.io/rosegoldwebdesign/tables/">YouTube Video Page</a></li>
-                                <li><a target="_blank" href="https://micah-rose.github.io/rosegoldwebdesign/image-map/">Image Map</a></li>
-                                <li><a target="_blank" href="https://micah-rose.github.io/rosegoldwebdesign/bootstrap-1/">Simply Me (Bootstrap)</a></li>
-                                <li><a target="_blank" href="https://micah-rose.github.io/rosegoldwebdesign/bootstrap-2/">Band (Bootstrap)</a></li>
-                            </ul>
-                          </div>
-                        </div>
-                        <div class="col-sm-4">
-                          <a href="#demo2" data-toggle="collapse">
-                            <img src={css} alt="CSS" width="200" height="200" />
-                          </a>
-                          <div id="demo2" class="collapse">
-                            <ul style={{
-                                textDecoration: "none", 
-                                listStyleType: "none"}}>
-                                <li><a target="_blank" href="https://micah-rose.github.io/rosegoldwebdesign/little-boxes/">Little Boxes</a></li>
-                                <li><a target="_blank" href="https://micah-rose.github.io/rosegoldwebdesign/pizza-2/">Pizza Parlor</a></li>
-                                <li><a target="_blank" href="https://micah-rose.github.io/rosegoldwebdesign/1430-midterm/">Eurypterids Midterm</a></li>
-                            </ul>
-                          </div>
-                        </div>
-                        <div class="col-sm-4">
-                            <a href="#demo3" data-toggle="collapse">
-                              <img src={js} alt="JavaScript" width="160" height="210" />
-                            </a>
-                            <div id="demo3" class="collapse">
-                              <ul style={{
-                                textDecoration: "none", 
-                                listStyleType: "none"}}>
-                                <li><a target="_blank" href="https://micah-rose.github.io/rosegoldwebdesign/js-birthday/">Happy Birthday</a></li>
-                                <li><a target="_blank" href="https://micah-rose.github.io/rosegoldwebdesign/mad-lib/">Mad Libs</a></li>
-                                <li><a target="_blank" href="https://micah-rose.github.io/rosegoldwebdesign/lottery/">Lottery</a></li>
-                                <li><a target="_blank" href="https://micah-rose.github.io/rosegoldwebdesign/guess/">Guessing Game</a></li>
-                                <li><a target="_blank" href="https://micah-rose.github.io/rosegoldwebdesign/tic-tac-toe/">Tic Tac Toe</a></li>
-                              </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <br />
-                <p style={{fontSize:"25px", marginBottom: "15px"}}>Here are some projects I wrote in Java and converted to JavaScript.</p>
-                <p>---Insert project links---</p>
+            <What />
             </div>
         </>
     )
--- src/components/What.js
+++ src/components/What.js
@@ -4,6 +4,70 @@ const What = () => {
     return (
         <>
             <h3 className="about-h3">W H A T</h3>
+            <br />
+            <div className="about-content">
+            <div className="what">
+                <p>Diving head first into the tech world was both scary and exhilarating, but it has
+                also been the most fun. There's this overwhelming sense of relief knowing that the career 
+                choice you've made is one you genuinely enjoy and feel passionate about. 
+                </p>
+                <br />
+                <p>Some of the skills I have gained over the years are:</p>
+                <br />
+                <p>This page displays some of the "whats" I've been working on and the goals I have set 
+                for myself. I am excited to share these projects with you (I've chosen some of my recent 
+                favorites) and I hope they properly showcase my abilties as a software engineer. 
+                </p>
+            </div>
+
+            <div className="projects">
+            <div class="row text-center">
+                <div class="col-sm-4">
+                  <div class="thumbnail">
+                    <img src="paris.jpg" alt="Paris" />
+                    <p><strong>Project 1</strong></p>
+                    <p>Fri. 27 November 2015</p>
+                    <button class="btn">Buy Tickets</button>
+                  </div>
+                </div>
+                <div class="col-sm-4">
+                  <div class="thumbnail">
+                    <img src="newyork.jpg" alt="New York" />
+                    <p><strong>Project 2</strong></p>
+                    <p>Sat. 28 November 2015</p>
+                    <button class="btn">Buy Tickets</button>
+                  </div>
+                </div>
+                <div class="col-sm-4">
+                  <div class="thumbnail">
+                    <img src="sanfran.jpg" alt="San Francisco" />
+                    <p><strong>Project 3</strong></p>
+                    <p>Sun. 29 November 2015</p>
+                    <button class="btn">Buy Tickets</button>
+                  </div>
+                </div>
+            </div>
+            </div>
+
+            <br />
+            <div className="course-buttons"> 
+                <p>It's important to know where you're going, but it's just as important to 
+                know where you came from. Below are the web development portfolios I built while 
+                taking courses at the community college. Feel free to check them out!! Pretty 
+                cool to see how far I've come. 
+                </p>
+               <a 
+                className="btn btn-info" 
+                href="https://micah-rose.github.io/rosegoldwebdesign/" 
+                target="_blank" 
+                rel="noreferrer">CSIS 1430</a>
+               <a 
+                className="btn btn-info" 
+                href="https://goji-saurus.000webhostapp.com/" 
+                target="_blank" 
+                rel="noreferrer">CSIS 2440</a>
+            </div>
+            </div> 
         </>
     )
 }
