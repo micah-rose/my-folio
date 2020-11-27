05d8c1deff44a2099cca1dbfb550a9860673a206
Micah Lund <micah.r.lund@gmail.com>
2020-11-27 16:43:33 -0700

    Did some clean up for the Weather App details.

--- src/components/Modal3.js
+++ src/components/Modal3.js
@@ -11,12 +11,13 @@ const Modal3 = () => {
                   <h2>JS Weather App</h2>
                 </div>
                 <div class="modal-body">
-                  <p>JS Weather App was a quick project I did to develop a weather app that would display
-                  the current weather forecase based on a users geological location and making a call to a 
-                  weather API. What I liked most about this project is it gave me exposure to working with 
-                  an API and revealing how easy it can be. After I completed this app it influenced me to
-                  focus more on projects centered around APIs and how I can build them. The app is currently
-                  only taking in a user's location in development, not in production.</p>
+                  <p>JS Weather App was a quick project I did in vanilla JavaScript. It is a weather app that 
+                    displays the current weather forecase based on a users geological location and making a call to a 
+                  weather API. What I liked most about this project is it gave me exposure to working with an API and 
+                  revealing how easy it can be. After I completed this app it influenced me to focus more on projects 
+                  centered around APIs and how I can build them.</p> 
+                  
+                  <p><em>The app is currently only taking in a user's location in development, not in production.</em></p>
                 </div>
                 <div class="modal-footer">
                   <button type="submit" class="btn btn-danger btn-default pull-left" data-dismiss="modal" >
