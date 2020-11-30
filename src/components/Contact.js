93c7ede6e11601dc3ea7e4404c85145731195450
Micah Lund <micah.r.lund@gmail.com>
2020-11-30 10:49:37 -0700

    Cleaned up Contact page. Aside from some other code clean up and project development, this portfolio is cleared for viewing and ready for deployment.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,11 +8,12 @@ const Contact = () => {
           <div className="about-content">
             <p className="text-center">Thank you SO much for visiting my site!! I hope to see you back soon.</p>
             <iframe src="https://giphy.com/embed/8Bl38gdtUK7WDdi59y" 
+            title="Adios"
             width="480" 
             height="480" 
             margin="auto"
             frameBorder="0" 
-            class="giphy-embed" 
+            className="giphy-embed" 
             allowFullScreen></iframe><p>
             <a style={{color: "black", fontSize: "15px"}} href="https://giphy.com/gifs/foxhomeent-8Bl38gdtUK7WDdi59y">via GIPHY</a></p>
             <br />
