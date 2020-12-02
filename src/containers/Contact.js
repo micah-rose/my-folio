df526bbfa3fdf83c31be4d482bfa475c15491267
Micah Lund <micah.r.lund@gmail.com>
2020-12-02 15:51:00 -0700

    Moved the thank you message and iframe on the Contact page to its own component.

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ThankYou from '../components/ThankYou';
 
 const Contact = () => {
     return (
@@ -6,20 +7,11 @@ const Contact = () => {
           <h2 style={{fontSize: "80px"}}>Contact</h2>
             <hr />
           <div className="about-content">
-            <p className="text-center">Thank you SO much for visiting my site!! I hope to see you back soon.</p>
-            <iframe src="https://giphy.com/embed/8Bl38gdtUK7WDdi59y" 
-            title="Adios"
-            width="480" 
-            height="480" 
-            margin="auto"
-            frameBorder="0" 
-            className="giphy-embed" 
-            allowFullScreen></iframe><p>
-            <a style={{color: "black", fontSize: "15px"}} href="https://giphy.com/gifs/foxhomeent-8Bl38gdtUK7WDdi59y">via GIPHY</a></p>
+            <ThankYou />
             <br />
+
             <p>If you have any questions, feedback, or are interested in a 
             copy of my resume please reach out via my LinkedIn profile.</p>
-
             <div className="row test">
               <div className="col-md-4">
                 <p><span className="glyphicon glyphicon-phone"></span>: <a href="https://github.com/micah-rose" target="_blank" rel="noreferrer">GitHub</a></p>              
